6f11966b95de95af4ee87e507d918f0a2fd5a28d
dev_olagold <olagoldlanre@gmail.com>
2023-01-02 01:15:59 +0100

    initial commit

---
+++ drum-kit/script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('keydown', playSound);
+
+function playSound (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.currentTime = 0;
+    audio.play();
+    console.log(audio);
+}
