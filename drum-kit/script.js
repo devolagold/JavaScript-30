fb3d4c0a1dc726c67c7d35034a8c98a446aaa405
dev_olagold <olagoldlanre@gmail.com>
2023-01-02 01:23:15 +0100

    changes

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -5,5 +5,5 @@ function playSound (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     audio.currentTime = 0;
     audio.play();
-    console.log(audio);
+    // console.log(audio);
 }
